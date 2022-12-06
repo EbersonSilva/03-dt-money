cf59c14887e557e3b8923456adb62948c2743e66
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2022-12-05 22:58:54 -0300

    Configurações finais do formulario de nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,35 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal(){
   return(
     <Dialog.Portal>
-          <Dialog.Overlay/>
+      <Overlay/>
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+      <CloseButton>
+        <X size={24}/>
+      </CloseButton>
+          <form action="">
+            <input type="text" placeholder='Descrição' required />
+            <input type="number" placeholder='Preço' required />
+            <input type="text" placeholder='Categoria' required />
+            <TransactionType>
+              <TransactionTypeButton variant='income'>
+                <ArrowCircleUp size={24}/>
+                Entrada
+              </TransactionTypeButton>
 
-          <Dialog.Content>
-            <Dialog.Title>Nova transação</Dialog.Title>
-            <Dialog.Close/>
-          </Dialog.Content>
-          
-        </Dialog.Portal>
+              <TransactionTypeButton variant='outcome'>
+                <ArrowCircleDown size={24}/>
+                Saida
+              </TransactionTypeButton>
+            </TransactionType>
+            <button type='submit'>Cadastrar</button>
+          </form>
+       
+      </Content>
+    </Dialog.Portal>
   )
 }

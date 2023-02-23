0fde8316cbe69abe1057d06a587a60f20a97cab8
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2023-02-23 11:54:27 -0300

    Criação do component de Summary e estilização

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,36 @@
-import { SummaryContainer } from './styles'
+import { SummaryCard, SummaryContainer } from './styles'
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
 
 export function Summary() {
   return (
     <SummaryContainer>
-      <div>
+      {/* Card 1 */}
+      <SummaryCard>
         <header>
           <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-      </div>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      {/* card 2 */}
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </SummaryCard>
+
+      {/* card 3 */}
+      {/* Propriedade de cor atribuida para o card Total */}
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
     </SummaryContainer>
   )
 }

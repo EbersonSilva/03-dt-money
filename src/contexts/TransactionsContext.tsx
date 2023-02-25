114eab6e6710caf3c8e4b4cf03fd30f8417619cd
Eberson Silva <binho.ebs@gmail.com>
2023-02-25 18:45:26 -0300

    Atualização na pagina de context

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,41 @@
-import {createContext} from "react";
+import { Children, createContext, ReactNode, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
 
 
+interface TransactionContextType{
+  transactions: Transaction[],
+}
 
-const TransactionContext = createContext()
+interface TransactionsProviderProps{
+  children: ReactNode
+}
+
+export const TransactionContext = createContext({} as TransactionContextType)
+
+export function TransactionsProvider({children}:TransactionsProviderProps){
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+  return(
+    <TransactionContext.Provider value={{transactions:[]}}>
+      {children}
+    </TransactionContext.Provider>
+  )
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,28 +8,8 @@ import {
   TransactionsTable
 } from './styles'
 
-interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createdAt: string
-}
-
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+ 
 
   return (
     <div>

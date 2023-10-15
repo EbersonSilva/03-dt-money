1fff2fcbfd9b352b6e75ebf8f58f6f134cd7b7cc
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2023-10-15 16:54:41 -0300

    Delete Desktop directory

--- Desktop/byte3ti-main/byte3ti-main/js/form.js
+++
@@ -1,8 +0,0 @@
-fetch('form.html')
-  .then(res => res.text())
-  .then(text => {
-    let oldelem = document.querySelector('script#replace_with_form')
-    let newelem = document.createElement('div')
-    newelem.innerHTML = text
-    oldelem.parentNode.replaceChild(newelem, oldelem)
-  })
